b3d410c45ce2ab6b3d4b41f7820fc4fbf42a0174
archibate <1931127624@qq.com>
2024-09-16 00:18:53 +0800

    noensureinstall

--- lua/archvim/config/mason.lua
+++ lua/archvim/config/mason.lua
@@ -9,6 +9,6 @@ require("mason").setup({
 })
 
 require("mason-lspconfig").setup {
-    ensure_installed = { "clangd", "pyright", "lua_ls" },
-    automatic_installation = true,
+    -- ensure_installed = { "clangd", "pyright", "lua_ls" },
+    -- automatic_installation = true,
 }
