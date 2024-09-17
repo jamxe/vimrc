0f8877ebb2d36beb3a7649868f7afaf2296463d7
archibate <1931127624@qq.com>
2024-09-17 22:37:47 +0800

    tryfixmac

--- compile.sh
+++ compile.sh
@@ -76,17 +76,18 @@ echo '-- Extracting bundled data...'
 base64 -d < \$tmptgz | tar -zx
 test -f ./install_deps.sh || echo \"ERROR: cannot extract file, make sure you have base64 and tar working\"
 fix_nvim_appimage() {
-    \$SUDO mv /usr/\$1/nvim /usr/\$1/.nvim.appimage.noextract
-    echo 'x=\$\$; mkdir -p /tmp/_nvim_appimg_.\$x && bash -c \"cd /tmp/_nvim_appimg_.\$x && /usr/\$1/.nvim.appimage.noextract --appimage-extract > /dev/null 2>&1\" && /tmp/_nvim_appimg_.\$x/squashfs-root/AppRun \"\$@\"; x=\$?; rm -rf /tmp/_nvim_appimg_.\$x exit \$x' | \$SUDO tee /usr/\$1/nvim
-    \$SUDO chmod +x /usr/\$1/nvim
+    \$SUDO mv /usr/bin/nvim /usr/bin/.nvim.appimage.noextract
+    echo 'x=\$\$; mkdir -p /tmp/_nvim_appimg_.\$x && bash -c \"cd /tmp/_nvim_appimg_.\$x && /usr/bin/.nvim.appimage.noextract --appimage-extract > /dev/null 2>&1\" && /tmp/_nvim_appimg_.\$x/squashfs-root/AppRun \"\$@\"; x=\$?; rm -rf /tmp/_nvim_appimg_.\$x exit \$x' | \$SUDO tee /usr/bin/nvim
+    \$SUDO chmod +x /usr/bin/nvim
 }
 install_nvim() {
     echo \"-- NeoVim 0.9.1 or above not found, installing latest for you\"
     if [ \"x\$(uname -sm)\" = \"xLinux x86_64\" ]; then
         test -f ./nvim.appimage || curl -L https://github.com/neovim/neovim/releases/latest/download/nvim.appimage -o ~/.config/nvim/nvim.appimage
         \$SUDO chmod +x ./nvim.appimage
-        test -f /usr/bin/nvim && \$SUDO mv /usr/bin/nvim /tmp/.nvim-executable-backup \|\| true
-        (\$SUDO cp ./nvim.appimage /usr/bin/nvim && (/usr/bin/nvim --version || fix_nvim_appimage bin)) \|\| (\$SUDO cp ./nvim.appimage /usr/local/bin/nvim && (/usr/local/bin/nvim --version || fix_nvim_appimage local/bin))
+        test -f /usr/bin/nvim && \$SUDO mv /usr/bin/nvim /tmp/.nvim-executable-backup || true
+        \$SUDO cp ./nvim.appimage /usr/bin/nvim
+        /usr/bin/nvim --version || fix_nvim_appimage
     elif [ \"x\$(uname -s)\" = \"xDarwin\" ]; then
         brew uninstall neovim 2> /dev/null || true
         brew install neovim
