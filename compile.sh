e26fd7e473eb2b14ea8f2d90029d998ac389ad7b
archibate <1931127624@qq.com>
2024-09-17 22:31:35 +0800

    comp

--- compile.sh
+++ compile.sh
@@ -76,17 +76,21 @@ echo '-- Extracting bundled data...'
 base64 -d < \$tmptgz | tar -zx
 test -f ./install_deps.sh || echo \"ERROR: cannot extract file, make sure you have base64 and tar working\"
 fix_nvim_appimage() {
-    \$SUDO mv /usr/bin/nvim /usr/bin/.nvim.appimage.noextract
-    echo 'x=\$\$; mkdir -p /tmp/_nvim_appimg_.\$x && bash -c \"cd /tmp/_nvim_appimg_.\$x && /usr/bin/.nvim.appimage.noextract --appimage-extract > /dev/null 2>&1\" && /tmp/_nvim_appimg_.\$x/squashfs-root/AppRun \"\$@\"; x=\$?; rm -rf /tmp/_nvim_appimg_.\$x exit \$x' | \$SUDO tee /usr/bin/nvim
-    \$SUDO chmod +x /usr/bin/nvim
+    \$SUDO mv /usr/\$1/nvim /usr/\$1/.nvim.appimage.noextract
+    echo 'x=\$\$; mkdir -p /tmp/_nvim_appimg_.\$x && bash -c \"cd /tmp/_nvim_appimg_.\$x && /usr/\$1/.nvim.appimage.noextract --appimage-extract > /dev/null 2>&1\" && /tmp/_nvim_appimg_.\$x/squashfs-root/AppRun \"\$@\"; x=\$?; rm -rf /tmp/_nvim_appimg_.\$x exit \$x' | \$SUDO tee /usr/\$1/nvim
+    \$SUDO chmod +x /usr/\$1/nvim
 }
 install_nvim() {
     echo \"-- NeoVim 0.9.1 or above not found, installing latest for you\"
-    test -f ./nvim.appimage || curl -L https://github.com/neovim/neovim/releases/latest/download/nvim.appimage -o ~/.config/nvim/nvim.appimage
-    \$SUDO chmod +x ./nvim.appimage
-    test -f /usr/bin/nvim && \$SUDO mv /usr/bin/nvim /tmp/.nvim-executable-backup
-    \$SUDO cp ./nvim.appimage /usr/bin/nvim
-    nvim --version || fix_nvim_appimage
+    if [ \"x\$(uname -sm)\" = \"xLinux x86_64\" ]; then
+        test -f ./nvim.appimage || curl -L https://github.com/neovim/neovim/releases/latest/download/nvim.appimage -o ~/.config/nvim/nvim.appimage
+        \$SUDO chmod +x ./nvim.appimage
+        test -f /usr/bin/nvim && \$SUDO mv /usr/bin/nvim /tmp/.nvim-executable-backup \|\| true
+        (\$SUDO cp ./nvim.appimage /usr/bin/nvim && (/usr/bin/nvim --version || fix_nvim_appimage bin)) \|\| (\$SUDO cp ./nvim.appimage /usr/local/bin/nvim && (/usr/local/bin/nvim --version || fix_nvim_appimage local/bin))
+    elif [ \"x\$(uname -s)\" = \"xDarwin\" ]; then
+        brew uninstall neovim 2> /dev/null || true
+        brew install neovim
+    fi
 }
 echo '-- Checking NeoVim version...'
 if which nvim; then
@@ -115,9 +119,13 @@ echo '-- Synchronizing packer.nvim...'
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerClean'
 echo '-- Copying language supports...'
-mkdir -p ~/.local/share/nvim/site/pack/packer/start/nvim-treesitter/parser
-mv ~/.config/nvim/nvim-treesitter-parser/*.so ~/.local/share/nvim/site/pack/packer/start/nvim-treesitter/parser/
-rmdir ~/.config/nvim/nvim-treesitter-parser || true
+if [ \"x\$(uname -sm)\" != \"xLinux x86_64\" ]; then
+    mkdir -p ~/.local/share/nvim/site/pack/packer/start/nvim-treesitter/parser
+    mv ~/.config/nvim/nvim-treesitter-parser/*.so ~/.local/share/nvim/site/pack/packer/start/nvim-treesitter/parser/
+else
+    echo 'WARNING: Not x86_64 linux, may need manually run :TSInstall cpp later!'
+fi
+rm -rf ~/.config/nvim/nvim-treesitter-parser
 echo '-- Copying mason registries...'
 mkdir -p ~/.local/share/nvim/mason/github/mason-org/mason-registry
 mv ~/.config/nvim/mason-registry/* ~/.local/share/nvim/mason/github/mason-org/mason-registry/
@@ -132,7 +140,7 @@ if [ ! -f ~/.clang-format ]; then
 fi
 echo '-- Verifying treesitters...'
 nvim --headless -c \"TSInstallInfo\" -c 'sleep 1 | q!' 2>&1 | grep -v 'not installed' || echo 'no installed treesitters?'
-if [ \"\$(uname -sm)\" != \"Linux x86_64\" ]; then
+if [ \"x\$(uname -sm)\" != \"xLinux x86_64\" ]; then
     for x in ${treesitters[*]}; do
         nvim --headless -c \"TSUpdateSync \$x\" -c 'sleep 1 | q!'
     done
